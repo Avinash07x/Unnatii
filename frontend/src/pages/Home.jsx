479a315a49e8d1b9ca6b10f157bbc8343ee0241e
avinashx53 <avinashsharma53011@gmail.com>
2025-09-15 21:05:28 +0530

    Update project with latest changess

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Properties from '../components/Properties'
-import Testimonials from '../components/Testimonials.JSX'
+import Testimonials from '../components/Testimonials.jsx'
 import FAQ from '../components/FAQ'
 
 
